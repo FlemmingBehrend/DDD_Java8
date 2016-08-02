7ea7022b57f8a753cb6e582352c305998faf91a1
FlemmingBehrend <feb@topdanmark.dk>
2016-08-02 12:30:39 +0200

    Refactored CarTest

--- src/test/java/dk/feb/ddd/domain/model/car/CarTest.java
+++ src/test/java/dk/feb/ddd/domain/model/car/CarTest.java
@@ -1,82 +1,104 @@
 package dk.feb.ddd.domain.model.car;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import javax.validation.ConstraintViolationException;
 import java.math.BigDecimal;
 
-@Disabled
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("A car")
 class CarTest {
 
+    @Nested
+    @DisplayName("when created")
+    class Creation {
 
-    @Test
-    public void weCanBuildAnInvalidCarWhenWeDoNotValidate() {
-        new Car.Factory(null, null)
-                .withColor("AW", Boolean.FALSE)
-                .withPrice(new BigDecimal(100000))
-                .withTopSpeed(301)
-                .buildWithoutValidation();
-    }
+        @Test
+        @DisplayName("can be build as an invalid instance if we do not validate")
+        void weCanBuildAnInvalidCarWhenWeDoNotValidate() {
+            assertNotNull(
+                    new Car.Factory(null, null)
+                            .withColor("AW", Boolean.FALSE)
+                            .withPrice(new BigDecimal(100000))
+                            .withTopSpeed(301)
+                            .buildWithoutValidation()
+            );
+        }
 
-    @Test
-    public void weCanNotBuildAnInvalidCarWhenWeValidate() {
-          // Expecting 5 validation errors when creating car with 5 errors
-//        exception.expect(ConstraintViolationException.class);
-//        exception.expect(numberOfConstraints(5));
-        new Car.Factory(new CarId("123"), Brand.FORD)
-                .withTopSpeed(null)
-                .withColor("AA", null)
-                .withPrice(BigDecimal.valueOf(-2))
-                .build();
-    }
+        @Test
+        @DisplayName("can not be build as an invalid instance if we validate")
+        void weCanNotBuildAnInvalidCarWhenWeValidate() {
+            ConstraintViolationException cve = expectThrows(
+                    ConstraintViolationException.class, () -> {
+                        new Car.Factory(new CarId("123"), Brand.FORD)
+                                .withTopSpeed(null)
+                                .withColor("AA", null)
+                                .withPrice(BigDecimal.valueOf(-2))
+                                .build();
+                    });
+            assertEquals(5, cve.getConstraintViolations().stream().count());
+        }
 
-    @Test
-    public void weCanBuildCarWithValidationIfAllValuesAreValid() {
-        Car car = new Car.Factory(new CarId("1234567"), Brand.MAZDA)
-                .withColor("Blue", Boolean.TRUE)
-                .withPrice(BigDecimal.valueOf(259000.00))
-                .withTopSpeed(220)
-                .build();
+        @Test
+        @DisplayName("can build as a valid instance if all values are valid")
+        public void weCanBuildCarWithValidationIfAllValuesAreValid() {
+            Car car = new Car.Factory(new CarId("1234567"), Brand.MAZDA)
+                    .withColor("Blue", Boolean.TRUE)
+                    .withPrice(BigDecimal.valueOf(259000.00))
+                    .withTopSpeed(220)
+                    .build();
 
-        // Expecting that all values have been set on aggregate
-//        assertEquals(new CarId("1234567"), car.getId());
-//        assertEquals(Brand.MAZDA, car.getBrand());
-//        assertEquals(new TopSpeed(220), car.getTopSpeed().get());
-//        assertEquals(BigDecimal.valueOf(259000.00), car.getPrice().get());
-//        assertEquals("Blue", car.getColor().get().getValue());
-    }
+            assertEquals(new CarId("1234567"), car.getId());
+            assertEquals(Brand.MAZDA, car.getBrand());
+            assertEquals(new TopSpeed(220), car.getTopSpeed().get());
+            assertEquals(BigDecimal.valueOf(259000.00), car.getPrice().get());
+            assertEquals("Blue", car.getColor().get().getValue());
+        }
 
+        @Nested
+        @DisplayName("use it's set methods")
+        class ChangeState {
 
+            @Test
+            @DisplayName("to change state")
+            public void weCanChangeStateOnCarEntityAfterItIsCreated() {
+                Car car = new Car.Factory(new CarId("1234567"), Brand.VOLVO)
+                        .withPrice(BigDecimal.TEN)
+                        .build();
+                assertEquals(Brand.VOLVO, car.getBrand());
+                car.setBrand(Brand.MAZDA);
+                assertEquals(Brand.MAZDA, car.getBrand());
+            }
 
-    @Test
-    public void weCanChangeStateOnCarEntityAfterItIsCreated() {
-        Car car = new Car.Factory(new CarId("1234567"), Brand.VOLVO)
-                .withPrice(BigDecimal.TEN)
-                .build();
-//        assertEquals(Brand.VOLVO, car.getBrand());
-        car.setBrand(Brand.MAZDA);
-//        assertEquals(Brand.MAZDA, car.getBrand());
-    }
+            @Test
+            @DisplayName("to validate with bean validation")
+            public void beanValidationIsCheckedWhenSetterMethodsAreCalled() {
+                Car car = new Car.Factory(new CarId("1234567"), Brand.VOLVO)
+                        .withPrice(BigDecimal.TEN)
+                        .build();
 
-    @Test
-    public void beanValidationIsCheckedWhenSetterMethodsAreCalled() {
-        Car car = new Car.Factory(new CarId("1234567"), Brand.VOLVO)
-                .withPrice(BigDecimal.TEN)
-                .build();
+                assertEquals(1,
+                        expectThrows(
+                                ConstraintViolationException.class, () -> car.setPrice(BigDecimal.valueOf(-1))
+                        )
+                                .getConstraintViolations().stream().count()
+                );
 
-        // Expecting setting of invalid value
-//        exception.expect(ConstraintViolationException.class);
-//        exception.expect(numberOfConstraints(1));
-        car.setPrice(BigDecimal.valueOf(-1));
+                assertEquals(1,
+                        expectThrows(
+                                ConstraintViolationException.class, () -> car.setBrand(null)
+                        )
+                                .getConstraintViolations().stream().count()
+                );
 
-        // Expecting setting of invalid value
-//        exception.expect(ConstraintViolationException.class);
-//        exception.expect(numberOfConstraints(1));
-        car.setBrand(null);
+                car.setTopSpeed(new TopSpeed(300));
+                assertEquals(300, car.getTopSpeed().get().getValue().intValue());
+            }
 
-        // Expecting no validation errors when setting valid valud
-        car.setTopSpeed(new TopSpeed(300));
+        }
     }
 
 }
